3faa3712cef0e49d0226de57e7ff4a6ed44ed235
He, Jiehui <he_jiehui@163.com>
2017-08-18 15:30:42 +0800

    fix line mess up issue when navigate in editor

--- com.xrosstools.xunit.editor/src/com/xrosstools/xunit/editor/parts/UnitAnchor.java
+++ com.xrosstools.xunit.editor/src/com/xrosstools/xunit/editor/parts/UnitAnchor.java
@@ -3,7 +3,6 @@ package com.xrosstools.xunit.editor.parts;
 import org.eclipse.draw2d.AbstractConnectionAnchor;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.geometry.Point;
-import org.eclipse.draw2d.geometry.PrecisionPoint;
 import org.eclipse.draw2d.geometry.Rectangle;
 
 public class UnitAnchor extends AbstractConnectionAnchor {
@@ -13,11 +12,14 @@ public class UnitAnchor extends AbstractConnectionAnchor {
 
 	public Point getLocation(Point loc)
 	{
-		Rectangle r = getOwner().getBounds();
+	    Rectangle r = new Rectangle(getOwner().getBounds());
+        
+        Point pos;
+        getOwner().translateToAbsolute(r);
+        
 		int x = loc.x < r.x ? r.x : r.x + r.width;
 		int y = r.y + r.height/2;
-		Point p = new PrecisionPoint(x,y);
-		getOwner().translateToAbsolute(p);
-		return p;
+		
+		return new Point(x, y);
 	}
 }
