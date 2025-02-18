910a28df14eb0515273139ed88e6823124c5f6e1
He, Jiehui <he_jiehui@163.com>
2025-02-18 17:17:07 +0800

    Release 2.6.1
    Fix editor can not be opened on MAC for IDEA 2024

--- com.xrosstools.xunit.idea.editor/src/com/xrosstools/xunit/idea/editor/XrossUnitEditorProvider.java
+++ com.xrosstools.xunit.idea.editor/src/com/xrosstools/xunit/idea/editor/XrossUnitEditorProvider.java
@@ -1,5 +1,8 @@
 package com.xrosstools.xunit.idea.editor;
 
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
+import com.intellij.openapi.application.TransactionGuard;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorPolicy;
@@ -20,7 +23,14 @@ public class XrossUnitEditorProvider implements FileEditorProvider, DumbAware {
     @NotNull
     @Override
     public FileEditor createEditor(@NotNull Project project, @NotNull VirtualFile virtualFile) {
-        FileDocumentManager.getInstance().saveAllDocuments();
+        ModalityState modalityState = ModalityState.defaultModalityState();
+
+        TransactionGuard.getInstance().submitTransaction(project, () ->
+            ApplicationManager.getApplication().invokeAndWait(() ->
+                FileDocumentManager.getInstance().saveAllDocuments()
+            , modalityState)
+        );
+
         return new XrossUnitEditor(project, virtualFile);
     }
 
